8bad04be188acadd6d8e09a9fea4079f4ceb204f
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-12-22 20:21:11 -0300

    Führt neue Anwendung zum Testen des Zerglieders für Spezifikationen.

---
+++ TestSpecsParser/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuelConsumption.TestSpecsParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string path in args)
+            {
+                try
+                {
+                    var hypertextFetcher = new Reusable.WebAccess.HypertextFetcher();
+                    Console.WriteLine($"Ladet '{path}' herunter...");
+                    string hypertext = hypertextFetcher.DownloadFrom(new Uri(path)).Result;
+
+                    var scraper = new WebCrawler.CarSpecsHypertextContentParser();
+                    IEnumerable<WebCrawler.CarSpecifications> parsedSpecs = scraper.ParseContent(hypertext);
+                    Console.WriteLine(
+                        $"Zergliederung hat die Spezifikationen von {parsedSpecs.Count()} Autos ergeben.");
+
+                    foreach (WebCrawler.CarSpecifications specs in parsedSpecs)
+                    {
+                        Console.WriteLine(specs.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Fehler bei der Zergliederung von '{path}' - {ex}");
+                }
+            }
+        }
+    }
+}
